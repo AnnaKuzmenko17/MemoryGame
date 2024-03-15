6cffbcf0e1b0785b5101fbca860a818d8222e582
ANNA KUZMENKO <anna.kuzmenko120063@gmail.com>
2024-03-16 01:39:11 +0200

    try

--- app.js
+++ app.js
@@ -1,51 +1,75 @@
 const cardArray = [
   {
-    name: 'fries',
-    img: 'img/fries.png'
+    name: 'princess-1',
+    img: 'img/princess-1.png'
   },
   {
-    name: 'cheeseburger',
-    img: 'img/cheeseburger.png'
+    name: 'princess-2',
+    img: 'img/princess-2.png'
   },
   {
-    name: 'milkshake',
-    img: 'img/milkshake.png'
+    name: 'princess-3',
+    img: 'img/princess-3.png'
   },
   {
-    name: 'pizza',
-    img: 'img/pizza.png'
+    name: 'princess-4',
+    img: 'img/princess-4.png'
   },
   {
-    name: 'hotdog',
-    img: 'img/hotdog.png'
+    name: 'princess-5',
+    img: 'img/princess-5.png'
   },
   {
-    name: 'ice-cream',
-    img: 'img/ice-cream.png'
+    name: 'princess-6',
+    img: 'img/princess-6.png'
   },
   {
-    name: 'fries',
-    img: 'img/fries.png'
+    name: 'princess-7',
+    img: 'img/princess-7.png'
   },
   {
-    name: 'cheeseburger',
-    img: 'img/cheeseburger.png'
+    name: 'princess-8',
+    img: 'img/princess-8.png'
   },
   {
-    name: 'milkshake',
-    img: 'img/milkshake.png'
+    name: 'princess-9',
+    img: 'img/princess-9.png'
   },
   {
-    name: 'pizza',
-    img: 'img/pizza.png'
+    name: 'princess-1',
+    img: 'img/princess-1.png'
   },
   {
-    name: 'hotdog',
-    img: 'img/hotdog.png'
+    name: 'princess-2',
+    img: 'img/princess-2.png'
   },
   {
-    name: 'ice-cream',
-    img: 'img/ice-cream.png'
+    name: 'princess-3',
+    img: 'img/princess-3.png'
+  },
+  {
+    name: 'princess-4',
+    img: 'img/princess-4.png'
+  },
+  {
+    name: 'princess-5',
+    img: 'img/princess-5.png'
+  },
+  {
+    name: 'princess-6',
+    img: 'img/princess-6.png'
+  },
+  {
+    name: 'princess-7',
+    img: 'img/princess-7.png'
+  },
+  {
+    name: 'princess-8',
+    img: 'img/princess-8.png'
+  },
+  {
+    name: 'princess-9',
+    img: 'img/princess-9.png'
   },
 ]
 
@@ -56,11 +80,15 @@ const gridDisplay = document.querySelector('#grid');
 
 function createBoard () {
   for (let i = 0; i < cardArray.length; i++) {
-    const card = document.createElement('img');
-    card.setAttribute('src', 'img/blank.png');
-    card.setAttribute('data-id', i);
-    card.addEventListener('click', flipCard);
-    gridDisplay.appendChild(card);
+    const base = document.createElement('div');
+    const img = document.createElement('img');
+    base.setAttribute('data-id', i);
+    img.setAttribute('data-id', i);
+    img.setAttribute('src', cardArray[i].img);
+    img.classList.add('transparent');
+    gridDisplay.appendChild(base);
+    base.appendChild(img);
+    img.addEventListener('click', flipCard)
   }
 }
 
@@ -73,20 +101,21 @@ let wonIds = [];
 
 function checkMatch () {
   const cards = document.querySelectorAll('img');
+  const bases = gridDisplay.children;
   let firstOption = chosenIds[0];
   let secondOption = chosenIds[1];
 
   if(chosenCards[0] === chosenCards[1]) {
-    cards[firstOption].setAttribute('src', 'img/white.png');
-    cards[secondOption].setAttribute('src', 'img/white.png');
+    bases[firstOption].setAttribute('class', 'match');
+    bases[secondOption].setAttribute('class', 'match');
     cards[firstOption].removeEventListener('click', flipCard);
     cards[secondOption].removeEventListener('click', flipCard);
     wonIds.push(chosenCards);
     result.innerHTML = wonIds.length;
     
   } else {
-    cards[firstOption].setAttribute('src', 'img/blank.png');
-    cards[secondOption].setAttribute('src', 'img/blank.png');
+    cards[firstOption].setAttribute('class', 'transparent');
+    cards[secondOption].setAttribute('class', 'transparent');
   }
   
   chosenCards = [];
@@ -94,10 +123,12 @@ function checkMatch () {
 }
 
 function flipCard() {
-  const cardId = this.getAttribute('data-id');
-  this.setAttribute('src', cardArray[cardId].img);
-  chosenCards.push(this.getAttribute('src'));
-  chosenIds.push(cardId);
+  const bases = grid.children;
+  const btnId = this.getAttribute('data-id');
+  this.classList.remove('transparent');
+  
+  chosenCards.push(cardArray[btnId].img);
+  chosenIds.push(btnId);
   
   if(chosenCards.length === 2) {
     setTimeout(checkMatch, 500);
@@ -112,9 +143,18 @@ refreshBtn.addEventListener('click', refreshGame);
 function refreshGame () {
   cardArray.sort(() => 0.5 - Math.random());
   const cards = document.querySelectorAll('img');
-  cards.forEach(card => card.setAttribute('src', 'img/blank.png'));
-  cards.forEach(card => card.addEventListener('click', flipCard));
+  const bases = document.querySelector('#grid').children;
+ 
+  for (let i = 0; i < cardArray.length; i++) {
+    bases[i].className = '';
+    cards[i].setAttribute('class', 'transparent');
+    cards[i].setAttribute('data-id', i);
+    cards[i].setAttribute('src', cardArray[i].img);
+    cards[i].addEventListener('click', flipCard)
+  };
+  
   wonIds = [];
   result.innerHTML = 0;
+  console.log(cardArray);
 }
 
